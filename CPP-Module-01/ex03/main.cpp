cc1d69c099485fc183b06cfd757b500aae1fa9ee
rcabezas <raul.29cg@gmail.com>
2021-11-19 22:19:12 +0100

    Making CPP01/ex04

--- CPP-Module-01/ex03/main.cpp
+++ CPP-Module-01/ex03/main.cpp
@@ -2,5 +2,19 @@
 
 int	main(void)
 {
-	
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
 }
---
+++ CPP-Module-01/ex04/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		std::cout << "Incorrect arguments" << std::endl;
+		return 0;
+	}
+}
