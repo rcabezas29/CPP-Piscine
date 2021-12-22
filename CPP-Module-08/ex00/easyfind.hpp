28d8a46b196f5cb340b97029c8ba58cf6b0ff99b
Raul Cabezas gonzalez <rcabezas@c1r2s5.42madrid.com>
2021-12-22 09:06:45 +0100

    Done CPP08/ex00

--- CPP-Module-08/ex00/easyfind.hpp
+++ CPP-Module-08/ex00/easyfind.hpp
@@ -6,12 +6,20 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 20:58:01 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/12/21 21:00:08 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/12/22 09:05:20 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template <typename T>
 void	easyfind(T t, int n)
 {
-	
+	for (unsigned long i = 0; i < t.size(); i++)
+	{
+		if (t[i] == n)
+		{
+			std::cout << n << " was find at " << i << " position" << std::endl;
+			return ;
+		}
+	}
+	throw 1;
 }
--- CPP-Module-08/ex00/main.cpp
+++ CPP-Module-08/ex00/main.cpp
@@ -6,11 +6,29 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 20:57:38 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/12/21 20:57:49 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/12/22 08:58:38 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include <iostream>
+#include "easyfind.hpp"
+
 int	main(void)
 {
-	
+	std::vector<int>	v;
+
+	for (int i = 0; i < 5; i++)
+		v.push_back(i * 2);
+
+	try
+	{
+		easyfind(v, 4);
+	}
+	catch (int)
+	{
+		std::cout << "The number couldn't be find" << std::endl;
+	}
+
+	return 0;
 }
