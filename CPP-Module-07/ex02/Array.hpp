b5f8aad5206f1f4fd48e10891398be9b90866d52
Raul Cabezas gonzalez <rcabezas@c1r8s5.42madrid.com>
2021-12-29 17:51:30 +0100

    Ready to be corrected CPP07

--- CPP-Module-07/ex02/Array.hpp
+++ CPP-Module-07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 20:56:29 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/12/29 11:42:21 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/12/29 17:50:46 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,19 @@ class	Array
 	public:
 		Array(void) : _array(new T[1]) {this->_array[0] = 0;}
 		Array(unsigned int n) : _array(new T[n + 1]){this->_array[n] = 0;}
-		Array(const Array &copy) : _array(copy._array){}
+		Array(const Array &copy) : _array(copy._array)
+		{
+			for (int i = 0; i < this->size(); i++)
+				this->_array[i] = copy._array[i];
+		}
+
 		virtual ~Array(void) {delete[] this->_array;}
 		Array	&operator=(const Array &op)
 		{
 			if (this == &op)
 				return *this;
-			this->_array = op._array;
+			for (int i = 0; i < this->size(); i++)
+				this->_array[i] = op._array[i];
 			return *this;
 		}
 		T	&operator[](size_t pos)
