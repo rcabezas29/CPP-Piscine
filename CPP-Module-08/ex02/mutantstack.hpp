97d4d3f3b25f9362d6bf4566ef7117fa7176cf6c
Raul Cabezas gonzalez <rcabezas@c1r8s5.42madrid.com>
2021-12-30 12:59:41 +0100

    Ready to correct CPP08

--- CPP-Module-08/ex02/mutantstack.hpp
+++ CPP-Module-08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 11:06:43 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/12/30 12:29:55 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/12/30 12:58:48 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ class MutantStack : public std::stack<T>
 		iterator	begin(void) {return std::stack<T>::c.begin();};
 		iterator	end(void) {return std::stack<T>::c.end();};
 
-		const_iterator	begin(void) {return std::stack<T>::c.begin();};
-		const_iterator	end(void) {return std::stack<T>::c.end();};
+		const_iterator	begin(void) const {return std::stack<T>::c.begin();};
+		const_iterator	end(void) const {return std::stack<T>::c.end();};
 };
