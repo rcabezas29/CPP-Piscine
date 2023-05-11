c6f61e9f41fa71569103a227ebacfea5afd08940
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2023-05-11 19:27:33 +0200

    Checked all cases on ex01

--- CPP-Module-09/ex01/RPN.cpp
+++ CPP-Module-09/ex01/RPN.cpp
@@ -5,47 +5,38 @@ RPN::RPN(std::string statement)
     for (unsigned long i = 0; i < statement.length(); ++i)
     {
         if (isdigit(statement[i]))
+        {
             this->st.push(statement[i] - '0');
-        else if (statement[i] == '+')
-        {
-            int b = this->st.top();
-            this->st.pop();
-            int a = this->st.top();
-            this->st.pop();
-            this->st.push(a + b);
-        }
-        else if (statement[i] == '-')
-        {
-            int b = this->st.top();
-            this->st.pop();
-            int a = this->st.top();
-            this->st.pop();
-            this->st.push(a - b);
-        }
-        else if (statement[i] == '*')
-        {
-            int b = this->st.top();
-            this->st.pop();
-            int a = this->st.top();
-            this->st.pop();
-            this->st.push(a * b);
-        }
-        else if (statement[i] == '/')
-        {
-            int b = this->st.top();
-            this->st.pop();
-            int a = this->st.top();
-            this->st.pop();
-            this->st.push(a / b);
+            continue ;
         }
         else if (isspace(statement[i]))
             continue ;
+        if (this->st.size() < 2)
+            throw std::runtime_error("Unable to do operation");
+        int b = this->st.top();
+        this->st.pop();
+        int a = this->st.top();
+        this->st.pop();
+        if (statement[i] == '+')
+            this->st.push(a + b);
+        else if (statement[i] == '-')
+            this->st.push(a - b);
+        else if (statement[i] == '*')
+            this->st.push(a * b);
+        else if (statement[i] == '/')
+        {
+            if (b == 0)
+                throw std::runtime_error("Trying to divide by zero");
+            this->st.push(a / b);
+        }
         else
         {
             std::cerr << "Error" << std::endl;
             exit(1);
         }
     }
+    if (this->st.size() != 1)
+        throw std::runtime_error("Invalid syntax");
     this->result = this->st.top();
 }
 
--- CPP-Module-09/ex01/main.cpp
+++ CPP-Module-09/ex01/main.cpp
@@ -4,9 +4,16 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        std::cerr << "ERROR" << std::endl;
+        std::cerr << "Error" << std::endl;
         return 1;
     }
-    RPN rpn_calculator(argv[1]);
-    std::cout << rpn_calculator.get_result() << std::endl;
+    try
+    {
+        RPN rpn_calculator(argv[1]);
+        std::cout << rpn_calculator.get_result() << std::endl;
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 }
